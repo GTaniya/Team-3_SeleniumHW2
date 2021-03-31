105bd13071f634d69febce9f593967a0b278be47
= <=>
2021-03-31 03:45:46 -0400

    First push

---
+++ src/TestTodayDeals.java
@@ -0,0 +1,55 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestTodayDeals {
+
+
+
+
+
+
+
+        static String relativePath = "/resources/drivers/windows/chromedriver.exe";
+        static String chromeDriverPath = System.getProperty("user.dir") + relativePath;
+        static WebDriver driver;
+
+        @Test
+
+        public void testDoSearch() throws InterruptedException {
+            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+            driver = new ChromeDriver();
+
+            driver.get("https://www.amazon.com/");
+            driver.manage().deleteAllCookies();
+            driver.manage().window().maximize();
+
+            WebElement searchBox =driver.findElement(By.id("twotabsearchtextbox"));
+            searchBox.sendKeys("sneakers");
+
+            WebElement searchButton =driver.findElement(By.id("nav-search-submit-button"));
+            searchButton.click();
+
+            String actualTitle = driver.getTitle().toLowerCase();
+            String expectedTitle = "Amazon.com : Sneakers".toLowerCase();
+
+            Assert.assertEquals(actualTitle, expectedTitle, "Title not found");
+
+
+            Thread.sleep(6000);
+
+            driver.close();
+            driver.quit(); // close executable program
+        }
+
+
+    }
+
+
+
+
+
+
